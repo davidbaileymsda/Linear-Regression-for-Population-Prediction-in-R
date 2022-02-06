0308dfdc309b904db62484abae6661c532e62eb8
David Bailey <davidbaileymsda@gmail.com>
2022-02-06 16:15:31 -0700

    Update R Code.R

--- R Code.R
+++ R Code.R
@@ -18,10 +18,25 @@ yearheaders <- c("2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017",
 
 Utah <- data.frame(print(Utahpopdata),yearheaders)
 
-
 #Plot the Utah data
 plot(Utah)
 
 #Gain Statistical Descriptors of Utah data
-head(utah)
+summary(utah)
  
+#Find regression line and add to plot
+regression <- lm(
+  formula = utahpop ~ year,
+  data = Utah)
+summary(regression)
+
+lines(utah$year,
+      fitted(regression),
+      col = "red")
+
+#Create data frame with years 2019-2025
+PredictYears <- data.frame(
+  year = c(2019, 2020, 2021, 2022, 2023, 2024, 2025))
+
+#Predict 5 more years of the population
+predict(regression, newdata = PredictYears)
